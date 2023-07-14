dbb8f0e56c5db48c9241acc41752cfbf80c87ee6
beni <szekrenyes.beni@gmail.com>
2023-07-14 22:07:20 +0300

    bootstrap project

---
+++ internshipLogisticTool/src/main/java/com/example/internshipLogisticTool/InternshipLogisticToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.internshipLogisticTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipLogisticToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipLogisticToolApplication.class, args);
+	}
+
+}
---
+++ internshipLogisticTool/src/test/java/com/example/internshipLogisticTool/InternshipLogisticToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internshipLogisticTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipLogisticToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
